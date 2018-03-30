706582100230e2f13fde127f89401143a24c09e5
cbelle <christabelle.chien@gmail.com>
2018-03-30 16:50:22 -0400

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,104 +1,146 @@
 $(document).ready(function() {
 // V A R I A B L E S 
-var timeRemaining = 10;
+var timeRemaining = 5;
 var interval;
-var intervalCount = 0;
-var intervalChange = true;
-var questions = ["who?","why?","where?","what?"];
-var choices = [["him","because","there","idk"],["q2a","q2b","q2c","q2d"]];
-var correctAnswers = ["A", "C"];
-// var indexOfCorrectAnswers = [];
-var correct;
+var questionCounter = 0;
+var questions = ["1why?", "2when?","3how?","4what?"];
+var answerChoices = [["a1", "b1", "c1", "d1"],["a2", "b2", "c2", "d2"],["a3","b3","c3","d3"],["a4","b4","c4","d4"]];
+var correctAnswers = ["a1", "b2", "c3", "d4"];
 var userAnswer;
+var correctGuesses = 0;
+var incorrectGuesses = 0;
+var unanswered = 0;
+var buttonClicked = false;
 
 // F U N C T I O N S
-//stores index positions of correct answers within the choices array
-// for (var i = 0; i < choices.length; i++) {
-//     indexOfCorrectAnswers[i] = choices[i].indexOf[correctAnswers[i]];
-//     }
-//decrements time remaining by 1
+//countdown timer
 function decrement() {
     timeRemaining--;
-    $("#timer").text("Time Remaining: " + timeRemaining);
+    $("#timer").text("Time remaining: " + timeRemaining);
+    //if time runs out, display time up
+    if (timeRemaining === 0) {
+        stop();
+        $("#question").text("Time's up!");
+        unanswered++;
+        displayAnswer();
+        displayNextQA();
+    }
 }
-//runs decrement function every second
-function countDown() {
+//decrement counter by 1 each second
+function countdown() {
     interval = setInterval(decrement, 1000);
 }
-//if user runs out of time or answers correctly/incorectly, this function will stop the clock, reset it, and keep track of the question #
+//stops timer
 function stop() {
     clearInterval(interval);
-    timeRemaining = 10;
 
 }
-//displays current question
-function showQuestion() {
-    $("#question").text(questions[intervalCount]);
+//displays question
+function displayQuestion() {
+    $("#question").text(questions[questionCounter]);
+    buttonClicked = false;
 }
 //displays current answer choices
 function showChoices() {
-    $("#choices").html("<button class='A'>"+choices[intervalCount][0]+"</button><button class='B'>"+choices[intervalCount][1]+"</button><button class='C'>"+choices[intervalCount][2]+"</button><button class='D'>"+choices[intervalCount][3]+"</button>");
-}
-//checks if user's choice is correct
-function checkAnswer() {
-    if (correctAnswers.indexOf(userAnswer) >= 0) {
-        correct = true;
-        correctAnswer();
-    } else {
-        correct = false;
-        incorrectAnswer();
+    for (var i = 0; i < 4; i++) {
+        var btn = $("<button>");
+        var answer = answerChoices[questionCounter];
+        btn.addClass("choices");
+        btn.attr("data-value", answer[i]);
+        btn.text(answer[i]);
+        $("#choices").append(btn);
     }
 }
-//displays correct answer feedback
-function correctAnswer() {
-    $("#question").text("CORRECT!");
-    $("."+indexOfCorrectAnswers[intervalCount]).addClass("correctAnswer");
+//check if answer is correct
+function checkAnswer() {
+    if (correctAnswers[questionCounter] === userAnswer) {
+        $("#question").text("CORRECT!");
+        correctGuesses++;
+        displayNextQA();
+    } else {
+        $("#question").text("WRONG!");
+        displayAnswer();
+        displayNextQA();
+        incorrectGuesses++;
+    }
 }
-//displays incorrect answer feedback
-function incorrectAnswer() {
-    $("#question").text("WRONG!");
-    $("#choices").html(correctAnswer[intervalCount-1]);
+//display correct answer
+function displayAnswer() {
+    $("#feedback").text("The correct answer was: " + correctAnswers[questionCounter]);
+}
+//clear previous answer buttons
+function clearChoices() {
+    $("#choices").empty();
+}
+//clear feedback
+function clearFeedback() {
+    $("#feedback").empty();
+}
+//pause for 3 seconds, then display next set of Q&A
+function displayNextQA(){
+    if (questionCounter < questions.length - 1) {
+        clearChoices();
+        setTimeout(reset, 3000);
+        setTimeout(displayQuestion, 3000);
+        setTimeout(showChoices, 3000);
+    } else {
+        stop();
+        setTimeout(playAgain, 3000);
+    }
+}
+//reset for next question
+function reset() {
+    timeRemaining = 5;
+    $("#timer").text("Time remaining: " + timeRemaining);
+    clearFeedback();
+    countdown();
+    questionCounter++;
+    buttonClicked = false;
+}
+//play again
+function playAgain() {
+    $("#timer").html("<button id='startOver'>Play again?</button>");
+    $("#question").text("Here's how you did:");
+    clearChoices();
+    $("#feedback").html("Correct: " + correctGuesses + "<br>Incorrect: " + incorrectGuesses + "<br>Unanswered: " + unanswered);
+}
+//reset game
+function gameReset() {
+    questionCounter = 0;
+    correctGuesses = 0;
+    incorrectGuesses = 0;
+    unanswered = 0;
+    buttonClicked = false;
+    $("#startOver").hide();
+    clearChoices();
+    clearFeedback();
+    timeRemaining = 5;
+}
 
-}
-while (intervalChange = false) {
-
-}
 // E V E N T S
-
-//on start button click, begin timer countdown, show first question
 $("#start").on("click", function() {
     $("#start").hide();
-    countDown();
-    showQuestion();
+    $("#timer").text("Time remaining: " + timeRemaining);
+    countdown();
+    displayQuestion();
     showChoices();
-    $("#timer").text("Time Remaining: " + timeRemaining);
-    $("#question").text();
-    $("#choices").text();
-    intervalChange = false;
-    //stops timer at 0
-    if (timeRemaining === 0) {
-    stop();
-    console.log(intervalCount);
-    showQuestion();
+});
+
+$(document).on("click", ".choices", function() {
+    userAnswer = $(this).attr("data-value");
+    buttonClicked = true;
+    if (buttonClicked === true) {
+        stop();
     }
-});
-
-$("#choices").on("click", function() {
-    userAnswer = $(this).attr("class");
-    console.log(userAnswer);
     checkAnswer();
-    console.log(intervalCount);
-        //if answer is correct
-        if (correct === true) {
-            stop();
-            correctAnswer();
-        } else if (correct === false) {
-            stop();
-            incorrectAnswer();
-        }
-    intervalCount++; //take out of this function?
 });
-
+$("#timer").on("click", function() {
+    gameReset();
+    $("#timer").text("Time remaining: " + timeRemaining);
+    countdown();
+    displayQuestion();
+    showChoices();
+});
 
 
 });
